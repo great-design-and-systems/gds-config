34ce702688ce50feaa285553d4f908b0a1938f21
deguzman.jerico.gadi <deguzman.jerico.gadi@gmail.com>
2016-11-14 12:31:27 -0500

    updates

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -5,72 +5,80 @@ import lodash from 'lodash';
 const LOGGER_PORT = process.env.LOGGER_SERVICE_PORT;
 const DOMAIN = process.env.DOMAIN_NAME || 'default';
 export default class Logger {
-    constructor(callback) {
-        new SetDefaultProtocol(LOGGER_PORT, (err, port) => {
-            this.port = port;
+  constructor(callback) {
+    new SetDefaultProtocol(LOGGER_PORT, (err, port) => {
+      this.port = port;
+    });
+    console.log('constructor', this);
+    new CheckAndGetApi(this.port, (err, actions) => {
+      if (err) {
+        console.error(err);
+        callback({
+          message: 'Failed getting logger service'
         });
-        console.log('constructor', this);
-        new CheckAndGetApi(this.port, (err, actions) => {
-            if (err) {
-                console.error(err);
-                callback({
-                    message: 'Failed getting logger service'
-                });
-            } else {
-                new AddServiceAction(actions.links, (errApi, result) => {
-                    global.gdsLogger = {
-                        logInfo: function (message) {
-                            result.createInfo.execute({
-                                params: {
-                                    serviceName: DOMAIN
-                                },
-                                data: {
-                                    message: message
-                                }
-                            }, () => {
-                                console.log(message);
-                            });
-                        },
-                        logError: function (message) {
-                            result.createError.execute({
-                                params: {
-                                    serviceName: DOMAIN
-                                },
-                                data: {
-                                    message: message
-                                }
-                            }, () => {
-                                console.error(message);
-                            });
-                        },
-                        logDebug: function (message) {
-                            result.createDebug.execute({
-                                params: {
-                                    serviceName: DOMAIN
-                                },
-                                data: {
-                                    message: message
-                                }
-                            }, () => {
-                                console.debug(message);
-                            });
-                        },
-                        logWarn: function (message) {
-                            result.createWarn.execute({
-                                params: {
-                                    serviceName: DOMAIN
-                                },
-                                data: {
-                                    message: message
-                                }
-                            }, () => {
-                                console.warn(message);
-                            });
-                        }
-                    };
-                    callback();
-                });
+      } else {
+        new AddServiceAction(actions.links, (errApi, result) => {
+          global.gdsLogger = {
+            logInfo: function(message) {
+              result.createInfo.execute({
+                params: {
+                  serviceName: DOMAIN
+                },
+                data: {
+                  message: getMessage(message)
+                }
+              }, () => {
+                console.log(message);
+              });
+            },
+            logError: function(message) {
+              result.createError.execute({
+                params: {
+                  serviceName: DOMAIN
+                },
+                data: {
+                  message: getMessage(message)
+                }
+              }, () => {
+                console.error(message);
+              });
+            },
+            logDebug: function(message) {
+              result.createDebug.execute({
+                params: {
+                  serviceName: DOMAIN
+                },
+                data: {
+                  message: getMessage(message)
+                }
+              }, () => {
+                console.debug(message);
+              });
+            },
+            logWarn: function(message) {
+              result.createWarn.execute({
+                params: {
+                  serviceName: DOMAIN
+                },
+                data: {
+                  message: getMessage(message)
+                }
+              }, () => {
+                console.warn(message);
+              });
             }
+          };
+          callback();
         });
-    }
+      }
+    });
+  }
+}
+
+function getMessage(message) {
+  if (message instanceof Error) {
+    return messsage.stack;
+  } else {
+    return message;
+  }
 }
