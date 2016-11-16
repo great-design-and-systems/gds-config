7dfbd6d17bb1cacaf96d9ffe210112e99707451b
rickzx98 <rickzx98@gmail.com>
2016-11-16 10:52:54 +0800

    updates

--- src/services/control/check-and-get-api.js
+++ src/services/control/check-and-get-api.js
@@ -4,25 +4,25 @@ const API_SERVICE_RETRY_TIMEOUT = process.env.API_SERVICE_RETRY_TIMEOUT || 5000;
 export default class CheckAndGetApi {
     constructor(url, callback) {
         console.log('Connecting to ', url);
-        this.retry = 0;
+        let retry = 0;
         restler.get(url, {
             timeout: API_SERVICE_RETRY_TIMEOUT
         }).on('success', function (data) {
             callback(undefined, data);
         }).on('error', function (reason) {
-            if (retry === RETRY_COUNT) {
+            if (retry === API_SERVICE_RETRY_COUNT) {
                 callback(reason);
             }
             retry++;
             this.retry(API_SERVICE_RETRY_TIMEOUT);
         }).on('fail', function (reason) {
-            if (retry === RETRY_COUNT) {
+            if (retry === API_SERVICE_RETRY_COUNT) {
                 callback(reason);
             }
             retry++;
             this.retry(API_SERVICE_RETRY_TIMEOUT);
         }).on('timeout', function (reason) {
-            if (retry === RETRY_COUNT) {
+            if (retry === API_SERVICE_RETRY_COUNT) {
                 callback(reason);
             }
             retry++;
