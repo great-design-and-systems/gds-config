e129f7b8d4bfdc2d72c06678489edba6e046a5b4
Jerico de Guzman <Jerico de Guzman>
2016-11-20 02:28:32 +0800

    updates

--- src/services/control/process-ports.js
+++ src/services/control/process-ports.js
@@ -7,28 +7,27 @@ import lodash from 'lodash';
 export default class ProcessPorts {
     constructor(servicePorts, restServices, callback) {
         batch(servicePorts).sequential().each((field, port, next) => {
-            this.serviceName = getServiceName(port);
-            console.log('Connecting to ' + this.serviceName + ':' + port.value);
+            const serviceName = getServiceName(port);
+            console.log('Connecting to ' + serviceName + ':' + port.value);
             new CheckAndGetApi(port.value, (apiErr, api) => {
                 if (apiErr) {
-                    console.log('failed connecting to ' + this.serviceName + ': ' + port.value);
+                    console.log('failed connecting to ' + serviceName + ': ' + port.value);
                     callback(apiErr);
                 } else {
-                    lodash.set(restServices, this.serviceName, api);
-                    console.log('Connected to ' + this.serviceName + ': ' + port.value);
-                    new AddServiceAction(api.links, (errLink, links) => {
+                    lodash.set(restServices, serviceName, api);
+                    console.log('Connected to ' + serviceName + ': ' + port.value);
+                    new AddServiceAction(api.links, (errLink, result) => {
                         if (errLink) {
                             console.error(errLink);
                             callback(errLink);
                         } else {
-                            lodash.set(lodash.get(restServices, this.serviceName), 'links', links);
                             next();
                         }
                     });
                 }
             });
         }).end(() => {
-            callback(undefined, servicePorts);
+            callback(undefined, restServices);
         });
     }
 }
