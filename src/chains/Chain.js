594e1ddd831501f19911ce7c065c55df6e8c5f81
rickzx98 <rickzx98@gmail.com>
2017-05-19 02:18:21 +0800

    updates

--- src/chains/Chain.js
+++ src/chains/Chain.js
@@ -1,12 +1,15 @@
 import lodash from 'lodash';
 
 export default class Chain {
-    constructor(name, action, next) {
+    constructor(name, action, next, error) {
         if (!action) {
             throw new Error('Action is required.');
         }
         this.next = next;
         this.context = new ChainContext();
+        if (error) {
+            this.context.set('$error', error);
+        }
         this.name = name;
         this.action = action;
         if (!global.chains) {
@@ -14,14 +17,36 @@ export default class Chain {
         }
         global.chains[this.name] = this;
     }
-    execute(prev) {
-        setTimeout(() => {
-            this.action(this.context, prev, () => {
-                if (this.next) {
-                    global.chains[this.next].execute(this.context);
+    execute(done, param) {
+        if ((param && param.$error) && !this.context.$error) {
+            this.context.set('$error', param.$error());
+        }
+        if (this.context.$isTerminated && this.context.$isTerminated()) {
+            done();
+        } else {
+            setTimeout(() => {
+                try {
+                    this.action(this.context, param, () => {
+                        if (this.next) {
+                            global.chains[this.next].execute(done, this.context);
+                        } else {
+                            done()
+                        }
+                    })
+                } catch (err) {
+                    if (this.context.$error) {
+                        this.context.set('$errorMessage', err);
+                        this.context.set('$name', this.name);
+                        global.chains[this.context.$error()].execute(done, this.context);
+                    } else {
+                        throw err;
+                    }
                 }
             })
-        })
+        }
+    }
+    terminate() {
+        this.context.set('$isTerminated', true);
     }
 }
 
